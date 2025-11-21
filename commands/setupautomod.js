32c56c686cfd562d338cdea28ee426560c23defa
matteeer <matteoparatore93@gmail.com>
2025-11-21 16:42:20 +0100

    Fixato il nome dello slash su automod

--- commands/setupautomod.js
+++ commands/setupautomod.js
@@ -11,7 +11,7 @@ import {
 import { ensureGuild, getGuildSettings } from '../utils/settings.js';
 
 export const data = new SlashCommandBuilder()
-  .setName('setup_automod')
+  .setName('setupautomod')
   .setDescription('Configura il sistema AutoMod di Nimbus.')
   .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
   .setDMPermission(false);
